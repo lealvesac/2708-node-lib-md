e44fcbc6b5c0a3af10d020c438d41e38d2f8b2b8
lealvesac <lealvesac@gmail.com>
2023-05-05 17:19:21 -0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-import chalk from "chalk";
+import fs from "fs";
 
-console.log(chalk.red('olá mundo')); 
+// import chalk from "chalk";
+
+function caseErro(erro) {
+    throw new Error(erro.code, "Erro na leitura do arquivo!");
+}
+
+function getFile(file) {
+    const encoding = "UTF-8";
+    fs.promises.readFile(file, encoding)
+    .then((texto) => console.log(texto))
+    .catch(caseErro);
+}
+
+// function getFile(file) {
+//     const encoding = "UTF-8";
+//     fs.readFile(file, encoding, (erro, texto) => {
+//         if(erro) {
+//             caseErro(erro);
+//         }
+//         console.log (texto);
+//     });
+// }
+
+getFile("./arquivos/texto.md")
